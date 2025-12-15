435d77e3072577239aa80fc785981f90b8b5bc1c
Grayson Gantek <grayson@inspry.com>
2025-12-15 15:18:09 -0500

    Add new filter for JWT leeway configuration

--- includes/class-checkview.php
+++ includes/class-checkview.php
@@ -80,7 +80,7 @@ class CheckView {
 		if ( defined( 'CHECKVIEW_VERSION' ) ) {
 			$this->version = CHECKVIEW_VERSION;
 		} else {
-			$this->version = '2.0.23';
+			$this->version = '2.0.24';
 		}
 		$this->plugin_name = 'checkview';
 
