dd8700c99ca63d23131782d8bd16d6e8b136d488
Grayson Gantek <grayson@inspry.com>
2026-01-21 10:31:25 -0500

    Add static id filter for Fluent Forms Terms and Conditions fields

--- includes/formhelpers/class-checkview-fluent-forms-helper.php
+++ includes/formhelpers/class-checkview-fluent-forms-helper.php
@@ -163,6 +163,12 @@ if ( ! class_exists( 'Checkview_Fluent_Forms_Helper' ) ) {
 			);
 
 			add_filter(
+				'fluentform/rendering_field_html_terms_and_condition',
+				array($this, 'static_ids'),
+				99
+			);
+
+			add_filter(
 				'fluentform/rendering_field_html_input_radio',
 				array($this, 'static_ids'),
 				99
