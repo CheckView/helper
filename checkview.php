401afe94604de457ff687136832e65a4f7b02df6
MichaelInspry <michael@inspry.com>
2026-03-30 12:44:26 -0400

    Designate Version For Branch

--- checkview.php
+++ checkview.php
@@ -11,7 +11,7 @@
  * Plugin Name:       CheckView
  * Plugin URI:        https://checkview.io
  * Description:       CheckView is the #1 fully automated solution to test your WordPress forms and detect form problems fast.  Automatically test your WordPress forms to ensure you never miss a lead again.
- * Version:           2.0.30
+ * Version:           2.0.30-elementor
  * Author:            CheckView
  * Author URI:        https://checkview.io/
  * License:           GPL-2.0+
