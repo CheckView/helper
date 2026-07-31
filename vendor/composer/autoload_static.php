469df666f3dc1e22dc3ec97386ffac49ccb79ec0
Grayson Gantek <grayson@inspry.com>
2026-07-31 10:41:43 -0400

    Replace boilerplate metadata in composer.json
    Set real package name, description, author (CheckView), and homepage;
    remove dead psr-4 autoload block (no src/ dir, namespace unused).
    Regenerates composer autoload files accordingly.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,10 +7,6 @@ namespace Composer\Autoload;
 class ComposerStaticInitcdf9877b6b6f16e6fbfbe3d480a3785e
 {
     public static $prefixLengthsPsr4 = array (
-        'c' =>
-        array (
-            'checkview\\' => 10,
-        ),
         'F' =>
         array (
             'Firebase\\JWT\\' => 13,
@@ -18,10 +14,6 @@ class ComposerStaticInitcdf9877b6b6f16e6fbfbe3d480a3785e
     );
 
     public static $prefixDirsPsr4 = array (
-        'checkview\\' =>
-        array (
-            0 => __DIR__ . '/../..' . '/src',
-        ),
         'Firebase\\JWT\\' =>
         array (
             0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
